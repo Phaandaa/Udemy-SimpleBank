9e03786eaf8871c51a948b9500796a5454f62f83
Davina Wijaya <phandavina@gmail.com>
2025-03-03 18:31:52 +0800

    add: transactions

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -6,6 +6,8 @@ import (
 	"fmt"
 )
 
+// TODO: ini masih banyak error tolong nanti cocokin sama file yg di github dulu
+
 // Store provides all functions to execute db queries and transactions
 type Store struct {
 	*Queries
@@ -33,6 +35,7 @@ func (store *Store) execTx(ctx context.Context, fn func(*Queries) error) error {
 		if rbErr := tx.Rollback(); rbErr != nil {
 			return fmt.Errorf("tx err: %v, rb err: %v", err, rbErr)
 		}
+		return err
 	}
 
 	return tx.Commit()
@@ -54,6 +57,7 @@ type TransferTxResult struct {
 	ToEntry     Entry    `json:to_entry`
 }
 
+// ini kaya service logic kalo di springboot
 // TransferTx performs a money transfer from one account to the other
 // It creates a trasnfer record, add account entries, and update accounts' balance withina single databse transaction
 func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error) {
@@ -61,11 +65,11 @@ func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (Trans
 
 	err := store.execTx(ctx, func(q *Queries) error {
 		var err error
-		
-		result.Transfer, err = q.CreateTransfer(ctx, CreateTransferParams {
+
+		result.Transfer, err = q.CreateTransfer(ctx, CreateTransferParams{
 			FromAccountID: arg.FromAccountID,
-			ToAccountID: arg.ToAccountID,
-			Amount: arg.Amount,
+			ToAccountID:   arg.ToAccountID,
+			Amount:        arg.Amount,
 		})
 
 		if err != nil {
@@ -74,24 +78,57 @@ func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (Trans
 
 		result.FromEntry, err = q.CreateEntry(ctx, CreateEntryParams{
 			AccountID: arg.FromAccountID,
-			Amount: -arg.Amount,
+			Amount:    -arg.Amount,
 		})
-
 		if err != nil {
 			return err
 		}
 
 		result.ToEntry, err = q.CreateEntry(ctx, CreateEntryParams{
 			AccountID: arg.ToAccountID,
-			Amount: arg.Amount,
+			Amount:    arg.Amount,
 		})
-
 		if err != nil {
 			return err
 		}
 
 		// TODO: update accounts' balance
-		
+
+		// get account -> update its balance
+
+		if arg.FromAccountID < arg.ToAccountID {
+			result.FromAccount, result.ToAccount, err = addMoney(ctx, q, arg.FromAccountID, -arg.Amount, arg.ToAccountID, arg.Amount)
+		} else {
+			result.ToAccount, result.FromAccount, err = addMoney(ctx, q, arg.ToAccountID, arg.Amount, arg.FromAccountID, -arg.Amount)
+		}
+
 		return nil
 	})
+
+	return result, err
+}
+
+func addMoney(
+	ctx context.Context, 
+	q *Queries,
+	accountID1 int64,
+	amount1 int64,
+	accountID2 int64,
+	amount2 int64,
+) (account1 Account, account2 Account, err error) {
+	account1, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
+		ID: accountID1,
+		Amount: amount1,
+	})
+
+	account2, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
+		ID: accountID2,
+		Amount: amount2,
+	})
+
+	if err != nil {
+		return // same as return account1, account2, err
+	}
+
+	return
 }
