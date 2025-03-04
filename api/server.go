c1cdbd17f2b1bbc79debc6cb3bd00ec64b09f465
Davina Wijaya <phandavina@gmail.com>
2025-03-04 14:09:16 +0800

    add: server endpoints

--- api/server.go
+++ api/server.go
@@ -10,3 +10,23 @@ type Server struct {
 	router *gin.Engine
 }
 
+func NewServer(store *db.Store) *Server {
+	server := &Server{store: store}
+	router := gin.Default()
+
+	router.POST("/accounts", server.createAccount)
+	router.GET("/accounts/:id", server.getAccount)
+	router.GET("/accounts", server.listAccount)
+
+	server.router = router
+	return server
+}
+
+// Start runs the HTTP server on a specific address
+func (server *Server) Start(address string) error {
+	return server.router.Run(address)
+}
+
+func errorResponse(err error) gin.H {
+	return gin.H{"error": err.Error()}
+}
